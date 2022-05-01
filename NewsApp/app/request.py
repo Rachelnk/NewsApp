76b583b7f3c862df1c44bef465c2151a26d42e26
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-01 15:58:39 +0300

    Import urllib.request to creare connection to our API URL and send request.

--- NewsApp/app/request.py
+++ NewsApp/app/request.py
@@ -1,3 +1,8 @@
 from app import app
+import urllib.request, json
+from .models import news
+News =news.news
 #  getting api key
 api_key = app.config['NEWS_API_KEY']
+# Getting the news source base url
+base_url = app.config["NEWS_API_BASE_URL"]
