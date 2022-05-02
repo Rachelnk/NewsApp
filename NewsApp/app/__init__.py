939ca9e3722bde37e72ccff483dc162e4dfc12ab
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-02 20:10:56 +0300

    Create create_app function that takes the configuration setting key as an argument.

--- NewsApp/app/__init__.py
+++ NewsApp/app/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask
-from .config import DevConfig
+from .config import DevConfig , config_options
 from flask_bootstrap import Bootstrap
 
-app = Flask (__name__, instance_relative_config = True)
+bootstrap = Bootstrap ()
+def create_app(config_name):
+  app = Flask (__name__)
+  # Creating the app configurations
+  app.config.from_object(config_options[config_name])
+  # Initializing flask extensions
+  bootstrap.init_app(app)
+  # Will add the views and forms
 
+  return app
 # set up the configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
 #  initialize flask extensions
-bootstrap = Bootstrap(app)
-from app import views
-from app import error
+# bootstrap = Bootstrap(app)
+# from app import views
+# from app import error
