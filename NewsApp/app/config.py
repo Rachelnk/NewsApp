f8ab928d77d82fdd08be88f506c3b63280369a0f
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-02 18:57:26 +0300

    Import os module that will allow our app to interact with the os dependent functionality.

--- NewsApp/app/config.py
+++ NewsApp/app/config.py
@@ -1,4 +1,5 @@
 from instance.config import NEWS_API_KEY
+import os
 
 
 class Config:
@@ -7,7 +8,9 @@ class Config:
     '''
     NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}'
     NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v1/articles?source={}&apiKey={}'
-    NEWS_API_KEY ='e9bccdcb8e164e9d8e5420ea88db15c5'
+    NEWS_API_KEY =os.environ.get('NEWS_API_KEY')
+    #  'e9bccdcb8e164e9d8e5420ea88db15c5'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     '''
@@ -23,3 +26,7 @@ class DevConfig(Config):
 
     '''
     DEBUG = True
+config_options = {
+    'development' :DevConfig,
+    'production' :ProdConfig
+}
