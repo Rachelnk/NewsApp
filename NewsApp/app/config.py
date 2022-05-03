67a6507f2b7a3cc93ba1781c5fd0afb33fc04754
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-03 10:09:32 +0300

    Add Procfile.

--- NewsApp/app/config.py
+++ NewsApp/app/config.py
@@ -8,8 +8,8 @@ class Config:
     '''
     NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}'
     NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v1/articles?source={}&apiKey={}'
-    NEWS_API_KEY =os.environ.get('NEWS_API_KEY')
-    #  'e9bccdcb8e164e9d8e5420ea88db15c5'
+    # NEWS_API_KEY =os.environ.get('NEWS_API_KEY')
+    NEWS_API_KEY = 'e9bccdcb8e164e9d8e5420ea88db15c5'
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
